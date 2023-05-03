94c04dd1f05c8af3a88b8a84d805bc2362f512a4
Davide <email@gmail.com>
2023-05-03 09:00:43 +0200

    push test

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+import seaborn as sns
+
+
+
+
+def main():
+	
+
+
+if __name__ == '__main__':
+	main()
---
+++ pag1.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+
+
+def show_footer():
+    st.markdown("*ciao*")
+
+
+def main():
+
+
+
+    ###### footer #####################################
+    show_footer()
+
+if __name__ == "__main__":
+    main()
